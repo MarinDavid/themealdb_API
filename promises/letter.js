34c25459923e668d9865560b8b94811e0543dff3
David Marín <marindavid1399@gmail.com>
2022-01-01 14:07:50 +0100

    añadidos unos pocos estilos y un innerHTML, falta pulirlo

--- promises/letter.js
+++ promises/letter.js
@@ -3,13 +3,18 @@ const searchByLetter = () =>{
     var l = letter.value;
     const API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${l}`;
 
+    var main = document.getElementById("main");
+    let tpl;
+
     try{
         fetch(API_URL)
             .then (response => response.json())
             .then (({meals}) => {
-                meals.forEach(meal => {
-                    console.log(meal.strMeal)
-                });
+                meals.forEach( (/*meal*/) => {
+                    // console.log(meal.strMeal);
+                    tpl = meals.map ((meal) => `<li>${meal.strMeal}</li>`);   
+                })
+                main.innerHTML = `<ul>${tpl}</ul>`;
     })
 }
     catch(error){
